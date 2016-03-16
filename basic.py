15b6e9793b0e8b78480cec9b2a88e066901984d1
wdy0401 <wdy0401@gmail.com>
2016-03-16 22:13:16 +0800

    map reduce

--- basic.py
+++ basic.py
@@ -29,3 +29,51 @@ while n > 0:
     n = n - 2
 print(sum)
 
+d={'aa':1,"bb":2}
+print(d['aa'])
+print('aa' in d)
+print("aa",d.get('aa'))
+print('cc',d.get('cc',-1))
+d.pop('aa')
+print('aa',d.get('aa',-1))
+
+lst=[1,2,3,4,5,1]
+print(lst)
+s1=set(lst)
+print(s1)
+
+a=100
+print(hex(a))
+print(a)
+
+def fun1(x):
+	return x*x
+print(fun1(2))
+
+def plus(*x):
+	ret=0
+	for i in x :
+		ret=ret+i
+	return ret
+print(plus(1,2,3,4))
+
+print(lst)
+print(lst[0:3])
+
+a="abcdefg"
+print(a[3:6])
+
+d={'aa':1,"bb":2}
+print(list(d))
+for e in sorted(list(d)):
+	print(e,d[e])
+for e in sorted(list(d.keys())):#与直接使用d相同
+	print(e)
+for e in sorted(list(d.values())):
+	print(e)
+
+def myabs(x):
+	return abs(x)
+def add(x,y,f):
+	return f(x)+f(y)
+print(add(1,-1,myabs))
