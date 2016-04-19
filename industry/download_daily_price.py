3a0e95a9e4a508d00dc306cb7212fd5d121aedfc
wdy0401 <wdy0401@gmail.com>
2016-04-19 17:08:10 +0800

    wind via python

--- industry/download_daily_price.py
+++ industry/download_daily_price.py
@@ -3,7 +3,16 @@
 	#mkdir for industry
 		#data of each stock
 
-from WindPy import *
-w.start();
+#from WindPy import *
+#w.start();
 
-print(w.wsd("000159.SZ", "sec_name,close,pe_ttm", "2010-01-01", "2016-04-17", "PriceAdj=F"))
+a=w.wsd('000637.SZ','close,industry_sw,industry_swcode,indexcode_sw,industry_citic,industry_citiccode,industry_gx,industry2,industrycode,industryname','2016-04-01','2016-04-18','industryType=1','category=1','industryStandard=1','PriceAdj=F')
+
+column=len(a.Data)
+row=len(a.Data[1])
+for i in range(row):
+	print(a.Times[i],end=",")
+	for j in range(column):
+		print(a.Data[j][i],end=",")
+	print('')
+	
